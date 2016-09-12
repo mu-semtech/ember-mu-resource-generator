024dd18da25fdc52ae2d37c8e2ccde28de422b58
Erika Pauwels <erika.pauwels@gmail.com>
2016-09-12 20:17:12 +0200

    Fix find by id in store in show/edit route

--- blueprints/mu-resource/files/app/routes/__name__.js
+++ blueprints/mu-resource/files/app/routes/__name__.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   store: Ember.inject.service(),
   model(params) {
-    return this.store.find('<%= entityName %>', params.<%= entityPathVar %>_id );
+    return this.store.find('<%= entityName %>', params.id );
   }
 });
