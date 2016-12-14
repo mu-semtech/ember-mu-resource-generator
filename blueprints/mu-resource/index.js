a7963903ca9f2d5e4ef8aa87bc182f199aca73e8
nielsv <niels.vandekeybus@tenforce.com>
2016-12-14 11:10:03 +0100

    only install dependencies on install of addon

--- blueprints/mu-resource/index.js
+++ blueprints/mu-resource/index.js
@@ -73,8 +73,6 @@ module.exports = {
 	},
   afterInstall: function(options) {
 			updateRouter.call(this, 'add', options);
-			this.addPackageToProject('ember-promise-helpers', '~1.0');
-      return this.addPackageToProject('ember-data-table', '~0.3');
 			// Ember CLI expects to resolve a promise from these hooks when running the blueprint
   },
 
--- index.js
+++ index.js
@@ -5,5 +5,13 @@ module.exports = {
 		name: 'ember-mu-resource-generator',
 		isDevelopingAddon: function() {
 				return true;
-		}
+		},
+    afterInstall: function(options) {
+			updateRouter.call(this, 'add', options);
+			this.addPackageToProject('ember-i18n', '~4.3');
+			this.addPackageToProject('ember-promise-helpers', '~1.0');
+      return this.addPackageToProject('ember-data-table', '~0.3');
+			// Ember CLI expects to resolve a promise from these hooks when running the blueprint
+  },
+
 };
