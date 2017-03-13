d3cdeac8c609677088566ab39409a6d48bc1224d
nielsv <niels.vandekeybus@tenforce.com>
2017-03-13 11:41:36 +0100

    smarter query params

--- blueprints/mu-resource/files/app/routes/__plural_name__/index.js
+++ blueprints/mu-resource/files/app/routes/__plural_name__/index.js
@@ -9,13 +9,24 @@ export default Ember.Route.extend(DataTableRouteMixin, {
 	return "    '"+ name + "': {refreshModel: true},"
 }).join("\n") %>
   },
-  mergeQueryOptions: function(params) {
-    var mergedParams = {
+		mergeQueryOptions: function(params) {
+    var mergedParams = {};
 <%= relationships.map( function(relationship) {
-  var name = relationship.name;
-	return "    'filter[" + name + "][id]': params['" + name + "'],";
-}).join("\n") %>
-    };
+		var name = relationship.name;
+		var result = "if (! Ember.isEmpty(params['" + name + "'])) { \n" +
+			           "    mergedParams['filter[" + name + "][id]'] = params['" + name + "'];\n" +
+		             "}";
+		return result;
+}).join("\n") %>   
     return mergedParams;
   },
+ // see https://guides.emberjs.com/v2.10.0/routing/query-params/#toc_sticky-query-param-values
+ // sticky query params for relationships are undesirable at the moment	
+ resetController(controller, isExiting, transition) {
+    if (isExiting) {
+      // isExiting would be false if only the route's model was changing
+     controller.set('page', 0);
+<%= relationships.map( function(relationship) {	return "    controller.set('" + relationship.name +"', '');"}).join("\n") %>
+    }
+  }		
 });
