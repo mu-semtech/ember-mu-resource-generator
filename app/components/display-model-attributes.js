fe04eedc99557057e330b413cd92a56018b77ebe
nielsv <niels.vandekeybus@tenforce.com>
2017-03-27 14:34:41 +0200

    removed generic display-model attributes component

--- app/components/display-model-attributes.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-mu-resource-generator/components/display-model-attributes';
--- blueprints/mu-resource/files/app/components/display-__name__-attributes.js
+++ blueprints/mu-resource/files/app/components/display-__name__-attributes.js
@@ -1,26 +1,4 @@
 import Ember from 'ember';
 import DisplayModelAttributes from 'ember-mu-resource-generator/components/display-model-attributes';
 
-export default DisplayModelAttributes.extend({
-  // specify the attributes you would like displayed
-  // by default retrieves all attributes
-  // uses a component based on the type of the attribute (gr-{type})
-  // example:
-  // props: Ember.Object.create( {
-  //        attributeName: {
-	//				  valueComponent: "gr-string"
-	//				  content: model.get(attributeName)
-  //        }
-  // })
-
-  // specify the relationships you would like displayed
-  // by default retrieves all relationships
-  // example:
-  // rels: Ember.Object.create({
-  //       attributeName: {
-  //     		kind: 'belongsTo',
-  // 		  	itemRoute: "myRoute.show",
-  // 			  promise: that.get("model.myRelationship")
-  //     }
-  // })
-});
+export default Ember.Component.extend({});
